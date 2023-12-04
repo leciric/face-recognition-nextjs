6526db3aebe8ca0c770412986419fd17987226ca
Leandro Ciric <lecirics@gmail.com>
2023-12-03 22:11:39 -0300

    build: fixed

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -9,7 +9,7 @@ export function Loading({ isLoading, title }: LoadingProps) {
   if (!isLoading) return null
 
   return (
-    <div className="z-60 absolute left-0 top-0 h-screen w-screen">
+    <div className="absolute left-0 top-0 z-60 h-screen w-screen">
       <div className=" flex h-full w-full flex-col items-center justify-center gap-2 bg-white/20 p-4 backdrop-blur-sm">
         <RotateCw className="animate-spin"></RotateCw>
         <span className="text-base font-semibold ">{title}</span>
